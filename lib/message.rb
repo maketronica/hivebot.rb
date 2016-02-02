351f9c0f3704d93db964b71edc0b134231ab5436
Jonathan Garvin <jon@5valleys.com>
2016-02-01 18:25:49 -0700

    Parse keypairs and validate checksum

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,38 @@
 class Message
-  attr_reader :data
+  attr_reader :data, :body, :checksum
 
   def initialize(data)
     @data = data
+  end
+
+  def to_h
+    Hash.new.tap do |hash|
+      body.split('&').each do |pair|
+        k, v = pair.split('=')
+        hash[k] = v
+      end
+    end
+  end
+
+  def valid?
+    provided_checksum.to_i == computed_checksum
+  end
+
+  private
+
+  def computed_checksum
+    body && body.bytes.reduce(:+).modulo(256)
+  end
+
+  def body
+    matchdata && matchdata[1]
+  end
+
+  def provided_checksum
+    matchdata && matchdata[2]
+  end
+
+  def matchdata
+    @matchdata ||= data.match(/B\|(.+)\%([0-9]{3})\|E/)
   end
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,10 +2,38 @@ require 'spec_helper'
 require 'message'
 
 describe Message do
-  let(:data) { 'somekindadata' }
+  let(:data) { 'B|foo=1&bar=2&baz=3%079|E' }
   let(:message) { Message.new(data) }
 
   it 'instantiates' do
     expect(message).to be_a(Message)
+  end
+
+  describe '#to_h' do
+    it 'returns a correct hash' do
+      expect(message.to_h).to eq({'foo' => '1', 'bar' => '2', 'baz' => '3'})
+    end
+  end
+
+  describe '#valid?' do
+    context 'when it has a valid checksum' do
+      it 'is truthy' do
+        expect(message.valid?).to be_truthy
+      end
+    end
+
+    context 'when it has an invalid checksum' do
+      let(:data) { 'B|foo=1&bar=2&baz=3%078|E' }
+      it 'is falsey' do
+        expect(message.valid?).to be_falsey
+      end
+    end
+
+    context' when data is garbage' do
+      let(:data) { 'iamapileofgarbage' }
+      it 'is falsey' do
+        expect(message.valid?).to be_falsey
+      end
+    end
   end
 end
