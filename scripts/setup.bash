03206f8b8c79a00d1e35d213a967dba7026f6fc7
Jonathan Garvin <jon@5valleys.com>
2016-05-10 23:42:36 -0600

    Add watchdog for network connection

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -10,3 +10,5 @@ sudo cp ../scripts/init.rb /etc/init.d/hivebot
 sudo chmod 755 /etc/init.d/hivebot
 sudo update-rc.d hivebot defaults
 sudo service hivebot restart
+
+sudo cp ../scripts/watchdog.cron /etc/cron.d/hivebotwatchdog
